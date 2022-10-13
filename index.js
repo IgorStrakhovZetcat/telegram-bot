315923ad30f89d0a2bddf30971d6a42fd12cd9c0
Zetcat <zetcat13@gmail.com>
2022-10-13 11:43:03 +0300

    init

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-const TelegramBot = require('node-telegram-bot-api');
+import { TOKEN, WEB_URL } from './tokens.js'
+import TelegramBot from 'node-telegram-bot-api';
 
-const token = '5695263363:AAEVPwE94Z-uoq7EL8vrFHpE5Tut-Pf0jqo';
 
-const webUrl = 'https://vermillion-tartufo-e0ee25.netlify.app'
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(TOKEN, { polling: true });
 
 
 bot.on('message', async (msg) => {
-  const chatId = msg.chat.id;
-  const text = msg.text;
+    const chatId = msg.chat.id;
+    const text = msg.text;
 
 
-  if(text === '/start'){
-    await bot.sendMessage(chatId, 'Fill out the form', {
-        reply_markup: {
-            keyboard: [
-                [{text: 'Form', web_app: {url: webUrl + '/form'}}]
-            ]
-        }
-    })
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'Fill out the form', {
+            reply_markup: {
+                keyboard: [
+                    [{ text: 'Form', web_app: { url: WEB_URL + '/form' } }]
+                ]
+            }
+        })
 
-    await bot.sendMessage(chatId, 'Go to website', {
-        reply_markup: {
-            inline_keyboard: [
-                [{text: 'To order', web_app: {url: webUrl}}]
-            ]
-        }
-    })
-  }
-
-  if(msg?.web_app_data?.data) {
-    try {
-        const data = JSON.parse(msg?.web_app_data?.data)
-        await bot.sendMessage(chatId, 'Thank you! Your country: ' + data?.country + ', your street: ' + data?.street + ', ' + data?.subject)
-    } catch (error) {
-        console.log(error)
+        await bot.sendMessage(chatId, 'Go to website', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'To order', web_app: { url: WEB_URL } }]
+                ]
+            }
+        })
     }
-  }
+
+    if (msg?.web_app_data?.data) {
+        try {
+            const data = JSON.parse(msg?.web_app_data?.data)
+            await bot.sendMessage(chatId, 'Thank you! Your country: ' + data?.country + ', your street: ' + data?.street + ', ' + data?.subject + ' person')
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
 
 
